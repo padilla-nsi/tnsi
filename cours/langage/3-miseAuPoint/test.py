4cd457ffd98314a625b95c4e6773b11a15129052
Pskalou <pascal.padilla@gmail.com>
2021-10-05 10:32:45 +0200

    mise au point

---
+++ cours/langage/3-miseAuPoint/test.py
@@ -0,0 +1,29 @@
+def contient_doublon(t: list) -> bool:
+    pass
+
+def cree () -> list :
+    pass
+
+def contient(s: list, x: int) -> bool:
+    pass
+
+def ajoute(s: list, x: int) -> None:
+    pass
+
+def wrong() -> str:
+    return False
+
+def toto(a: int) -> bool:
+    return a*2
+
+from typing import List
+
+def test(tab: List[int]) -> int:
+    return True
+
+class Chrono:
+    def __init__(self):
+        pass
+    def texte(self) -> str:
+        return "12h 42min 42s"
+
