e7befa89b85c5daaee8c42105584734fd40b1925
Pskalou <pascal.padilla@gmail.com>
2021-12-10 13:00:22 +0100

    decoupage des sujets écrits en exercices

--- cours/donnees/2-basesDeDonneesRelationnelles/_coding.py
+++ cours/donnees/2-basesDeDonneesRelationnelles/_coding.py
@@ -1,10 +1,21 @@
-dico = {':loud_laugh:':'XD', ':slight_smile:':':)', ':stuck_out_tongue:':'p', ':open_mouth:':':o',  ':slight_smile:':':)',  ':disappointed:':':('} 
+# dico = {':loud_laugh:':'XD', ':slight_smile:':':)', ':stuck_out_tongue:':'p', ':open_mouth:':':o',  ':slight_smile:':':)',  ':disappointed:':':('} 
 
-ch = 'This is SOO easy :loud_laugh:! :disappointed:!!!'
+# ch = 'This is SOO easy :loud_laugh:! :disappointed:!!!'
 
-for tag, val in dico.items():
-    rank = ch.find(tag)
-    if rank != -1:
-        n = len(tag)
-        ch = ch[:rank] + val  + ch[rank+n:]
-        print(ch)
+# for tag, val in dico.items():
+#     rank = ch.find(tag)
+#     if rank != -1:
+#         n = len(tag)
+#         ch = ch[:rank] + val  + ch[rank+n:]
+#         print(ch)
+
+
+
+ch = -1
+
+
+if ch >= 0:
+    print(ch//100+1)
+else:
+    print(-(ch*-1 // 100)-1)
+n = 'machaine'
