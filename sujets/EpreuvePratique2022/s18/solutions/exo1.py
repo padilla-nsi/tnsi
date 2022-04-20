0d21c1c605b3ef2ab59657c440bfa5911c841be6
Pskalou <pascal.padilla@gmail.com>
2022-04-20 13:29:19 +0200

    sujet 18

--- sujets/EpreuvePratique2022/s18/solutions/exo1.py
+++ sujets/EpreuvePratique2022/s18/solutions/exo1.py
@@ -1,16 +1,66 @@
+"""
+Author: Pascal Padilla
+Source: correction de l'exercice 1 du sujet 18 des épreuves pratiques NSI 2022
+"""
+
+
+from doctest import testmod
+
+
 def mini(releve, date):
+    """
+    Fonction qui renvoie la plus petite valeur relevée dans le
+    tableau `releve` et l'année correspondante issue du tableau `date`.
+
+    Args:
+        releve (List[float]): tableau de températures (flottants)
+        date (List[int]): tableau de nombres entiers (les années)
+
+    Returns:
+        tuple: couple (valeur minimale, année associée)
+
+    Tests:
+    >>> t_moy = [14.9, 13.3, 13.1, 12.5, 13.0, 13.6, 13.7]
+    >>> annees = [2013, 2014, 2015, 2016, 2017, 2018, 2019]
+    >>> mini(t_moy, annees)
+    (12.5, 2016)
+    """
+    # BOUCLE
+    # invariant:
+    #   → `i_min`: est l'indice de la valeur minimale de la
+    #      portion de tableau releve[0 .. i-1]
+    # initialisation:
+    #   → `i_min`: est fixé à 0 (première case du tableau)
+    #   → `i`: indice courant de la boucle fixé à 1
     i_min = 0
-    v_min = releve[0]
 
-    n = len(releve)
-    for i in range(1, n):
-        if releve[i] < v_min:
+    # condition d'arrêt: tout le tableau parcouru
+    #   → i == len(releve)
+    nb_releves = len(releve)
+
+    for i in range(1, nb_releves):
+        # mise à jour de l'invariant si une valeur plus
+        # petite est trouvée
+        if releve[i] < releve[i_min]:
             i_min = i
-            v_min = releve[i]
-    return (releve[i_min], date[i_min])
+
+    # POST boucle
+    # i_min est l'indice de la valeur minimale
+    releve_min = releve[i_min]
+    date_min = date[i_min]
+    return (releve_min, date_min)
 
 
 
+# tests avec une assertion
 t_moy = [14.9, 13.3, 13.1, 12.5, 13.0, 13.6, 13.7]
 annees = [2013, 2014, 2015, 2016, 2017, 2018, 2019]
 assert mini(t_moy, annees) == (12.5, 2016)
+
+
+# tests avec des affichages
+print(mini(t_moy, annees))
+
+
+# tests avec doctest
+testmod()
--- sujets/EpreuvePratique2022/s18/solutions/exo2.py
+++ sujets/EpreuvePratique2022/s18/solutions/exo2.py
@@ -1,20 +1,53 @@
+"""
+Author: Pascal Padilla
+Source: correction de l'exercice 2 du sujet 18 des épreuves pratiques NSI 2022
+"""
+
+
 def inverse_chaine(chaine):
+    # initialisation avec une chaîne vide
     result = ''
+
+    # boucle qui parcourt les symboles de la chaîne de
+    # caractère un par un
+    # `caractete` vaut le symbole courant
     for caractere in chaine:
-       result = caractere + result
+
+        # concaténation du caractère courant au **début** de
+        # la chaîne résultat qui sera renvoyée
+        result = caractere + result
+
     return result
 
+
 def est_palindrome(chaine):
+    # inverse contient la chaîne inversée
     inverse = inverse_chaine(chaine)
+
+    # tests booléen qui vaut: 
+    # Vrai ←→ la chaine et son inverse sont égales
+    #
+    # la fonction renvoie le résultat du test booléen
     return chaine == inverse
-    
+
+
 def est_nbre_palindrome(nbre):
+    # transforme le nbre en chaîne de caractère
     chaine = str(nbre)
     return est_palindrome(chaine)
 
 
+# tests avec des assertions
 assert inverse_chaine('bac') == 'cab'
 assert est_palindrome('NSI') == False
 assert est_palindrome('ISN-NSI') == True
 assert est_nbre_palindrome(214312) == False
 assert est_nbre_palindrome(213312) == True
+
+
+# tests avec des affichages
+print(inverse_chaine('bac'))
+print(est_palindrome('NSI'))
+print(est_palindrome('ISN-NSI'))
+print(est_nbre_palindrome(214312))
+print(est_nbre_palindrome(213312))
