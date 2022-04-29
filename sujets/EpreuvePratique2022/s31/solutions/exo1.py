b67137b65a7b0eb20a15d95ed335d1beb1592f3a
Pskalou <pascal.padilla@gmail.com>
2022-04-29 17:25:28 +0200

    ajout commentaires

--- sujets/EpreuvePratique2022/s31/solutions/exo1.py
+++ sujets/EpreuvePratique2022/s31/solutions/exo1.py
@@ -1,12 +1,52 @@
-def recherche(a, t):
-    occurences = 0
-    for x in t:
-        if x == a:
-            occurences += 1
-    return occurences
+"""
+Author: Pascal Padilla
+Source: correction de l'exercice 1 du sujet 31 des épreuves pratiques NSI 2022
+
+Remarques:
+"""
+
+def recherche(element, tab):
+    """
+    Tests et exemples:
+    >>> recherche(5,[])
+    0
+    >>> recherche(5,[-2, 3, 4, 8])
+    0
+    >>> recherche(5,[-2, 3, 1, 5, 3, 7, 4])
+    1
+    >>> recherche(5,[-2, 5, 3, 5, 4, 5])
+    3
+    """
+    # BOUCLE
+    # -> invariant: occurrence contient le nb d'apparition
+    #               de element dans la zone déjà parcourue de tab
+    # -> initialisation: au départ, rien n'est parcouru
+    occurrence = 0
+
+    # -> condition d'arrêt: après la dernière case du tableau
+    for elt_courant in tab:
+        # maintien de l'invariant si la valeur element est trouvée
+        if elt_courant == element:
+            # incrémenter le compteur `occurrence`
+            occurrence = occurrence + 1
+    
+    # fin de BOUCLE
+    # tout le tableau est parcouru et occurrence est correct
+    return occurrence
 
 
+# Vérification par des assertions
 assert recherche(5,[]) == 0
 assert recherche(5,[-2, 3, 4, 8]) == 0
 assert recherche(5,[-2, 3, 1, 5, 3, 7, 4]) == 1
 assert recherche(5,[-2, 5, 3, 5, 4, 5]) == 3
+
+# Vérification par des affichages
+print(recherche(5,[]))
+print(recherche(5,[-2, 3, 4, 8]))
+print(recherche(5,[-2, 3, 1, 5, 3, 7, 4]))
+print(recherche(5,[-2, 5, 3, 5, 4, 5]))
+
+# Vérification par doctest
+from doctest import testmod
+testmod()
--- sujets/EpreuvePratique2022/s31/solutions/exo2.py
+++ sujets/EpreuvePratique2022/s31/solutions/exo2.py
@@ -1,17 +1,56 @@
+"""
+Author: Pascal Padilla
+Source: correction de l'exercice 1 du sujet 31 des épreuves pratiques NSI 2022
+
+Remarques:
+    * algo glouton classique:
+        un algorithme glouton est un algorithme qui fait un choix
+        glouton à chaque tour de boucle.
+        Ce choix peut éviter pleins de calculs.
+        Ici, le choix glouton c'est de rendre, quand c'est possible
+        la pièce la plus grande. Et si ne ce n'est pas possible, 
+        de rendre la pièce d'un montant juste inférieur.
+"""
+
+
 def rendu_monnaie_centimes(s_due, s_versee):
     pieces = [1, 2, 5, 10, 20, 50, 100, 200]
+    # -> initialisation: tableau vide (rien à rendre)
     rendu = []
+
+    # -> initialisation de la somme à rendre
     a_rendre = s_versee - s_due
+
+    # -> invariant: i est l'indice de la piece qu'on essaye de rendre
     i = len(pieces) - 1
+
+    # -> condition d'arrêt: il n'y a plus rien à rendre
     while a_rendre > 0 :
+        # il est possible de rendre une piece courante piece[i]
         if pieces[i] <= a_rendre :
+            # ajout de la pièce dans le tableau de l'argent rendu
             rendu.append(pieces[i])
+            # mise à jour de la somme à rendre
             a_rendre = a_rendre - pieces[i]
+        
+        # la pièce courante est plus grande que la somme à rendre
+        # on passe donc à une pièce d'un montant juste inférieur
         else :
             i = i - 1
+    
+    # fin BOUCLE
+    # il n'y a plus rien à rendre 
+    # et rendu contient les pièces rendues
     return rendu
 
 
+
+# Vérification par des assertions
 assert rendu_monnaie_centimes(452, 500) == [20, 20, 5, 2, 1]
 assert rendu_monnaie_centimes(700,700) == []
 assert rendu_monnaie_centimes(112,500) == [200, 100, 50, 20, 10, 5, 2, 1]
+
+# Vérification par des affichages
+print(rendu_monnaie_centimes(452, 500))
+print(rendu_monnaie_centimes(700,700))
+print(rendu_monnaie_centimes(112,500))
